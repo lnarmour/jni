2d6c54da71a0ab6c947cfae534d3edfe4d751c5d
Louis Narmour <lnarmour@rams.colostate.edu>
2019-05-22 15:14:34 -0600

    compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,13 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
+#export JAVA_HOME=$(/usr/libexec/java_home)
+
 javac *.java -h .
-gcc -g -dynamiclib -I/usr/include -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin multiply.c -o libTest.dylib
+
+# MacOS
+#gcc -g -dynamiclib -I/usr/include -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin multiply.c -o libTest.dylib
+
+# linux
+gcc -shared -c -g -I$JAVA_HOME/include \
+    -I$JAVA_HOME/include/linux \
+    -fPIC -MMD -MP -MF "multiply.o.d" -o multiply.o multiply.c
+gcc -shared -o multiply.so multiply.o -fPIC
 
